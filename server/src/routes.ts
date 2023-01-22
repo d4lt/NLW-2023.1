28fb0ddcb1ff2a60048b4cbf4829e0645c3a0bbe
d4lt <93852260+d4lt@users.noreply.github.com>
2023-01-22 16:48:11 -0300

    created /summary and /habits/id:/toggle routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -81,5 +81,87 @@ export async function appRoutes(app: FastifyInstance){
 
   })
 
+
+  // TODO: Especificar dia e restringir o toggle pelo weekDay
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleParam = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = toggleParam.parse(request.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    })
+
+    if (!day){
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id:{
+          day_id:day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit){
+      // descompletar o hábito
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+
+    } else {
+    // completar o hábito
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      })
+
+    }  
+
+  })
+
+  app.get('/summary', async (request) => {
+    // TODO: Abstrair a query de summary (atual é do SQLite)
+
+    const summary = await prisma.$queryRaw`
+      SELECT
+        D.id,
+        D.date,
+        (
+          SELECT cast(count(*) as float) 
+          FROM habits_week_days HWD
+          JOIN habits H 
+            ON H.id = HWD.habit_id
+          WHERE 
+            HWD.week_day = cast( strftime('%w', D.date/1000.0, 'unixepoch') as int) 
+            AND
+            H.created_at <= D.date
+        ) as available,
+
+        (
+          SELECT cast(count(*) as float) FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed
+      FROM days D
+    `
+
+    return summary;
+  })
 }
 
